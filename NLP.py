9c2e85a61c591e30027222121e99514d893f09a7
ikeshare192 <isaacshareef@hotmail.com>
2021-10-11 22:42:50 -0600

    Revamping this to include pdf reader and model processing

--- NLP.py
+++ NLP.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from transformers.pipelines import pipeline
 import PyPDF3 as pdf
-
+from transformers import pipeline
 
 st.cache(show_spinner=False)
+
+
 
 def load_model():
     question_answerer = pipeline('question-answering')
@@ -13,18 +14,24 @@ npl_pipe = load_model()
 
 st.header("Prototyping NLP Solution")
 st.text("This demo uses a model for Question Answering.")
+
+uploaded_file = st.file_uploader(
+    "upload pdfs",
+    type='pdf',
+    accept_multiple_files=False
+    )
+
+read_file = pdf.PdfFileReader(uploaded_file)
+page1 = read_file.getPage(0)
+text = page1.extractText()
+
+
 add_text_sidebar = st.sidebar.title("Menu")
 add_text_sidebar = st.sidebar.text("Just some random text.")
 question = st.text_input(label='Insert a question.')
-text = st.text_area(label="Context")
+#text = st.text_area(label="Context")
+
 
 if (not len(text)==0) and not (len(question)==0):
     x_dict = npl_pipe(max_answer_length=50, context=text,question=question)
     st.text(f"Answer: {x_dict['answer']}.")
-'''
-pdf_file_obj = open('paper.pdf', 'rb')
-read_file = pdf.PdfFileReader(pdf_file_obj)
-page1 = read_file.getPage(0)
-text = page1.extractText()
-print(text)
-'''
--- file_reader.py
+++ file_reader.py
@@ -3,12 +3,13 @@ import streamlit as st
 
 
 
-uploaded_file = st.file_uploader("upload pdfs", tupe=pdf,
-                                    accept_multiple_files=True,)
-'''
-pdf_file_obj = open('paper.pdf', 'rb')
-read_file = pdf.PdfFileReader(pdf_file_obj)
+uploaded_file = st.file_uploader(
+    "upload pdfs",
+    type='pdf',
+    accept_multiple_files=False
+    )
+
+read_file = pdf.PdfFileReader(uploaded_file)
 page1 = read_file.getPage(0)
 text = page1.extractText()
-print(text)
-'''
+#st.write(text)
