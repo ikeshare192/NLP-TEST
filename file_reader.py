d26c4614529099535ffabff2d79d52015e05b754
ikeshare192 <isaacshareef@hotmail.com>
2021-10-11 19:54:18 -0600

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,14 @@
 import PyPDF3 as pdf
+import streamlit as st
 
-#file = open('Machine Learning White Paper - Isaac Shareef.pdf', 'rb')
 
+
+uploaded_file = st.file_uploader("upload pdfs", tupe=pdf,
+                                    accept_multiple_files=True,)
+'''
 pdf_file_obj = open('paper.pdf', 'rb')
 read_file = pdf.PdfFileReader(pdf_file_obj)
 page1 = read_file.getPage(0)
 text = page1.extractText()
 print(text)
+'''
